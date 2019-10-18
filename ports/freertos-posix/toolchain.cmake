f1492bc5aea262198a476982bfdbb29f824729b9
Bart Poot <bart.poot@adlinktech.com>
2019-10-18 17:01:40 +0200

    Moved toolchain.cmake file to ports directory; adapted travis.yml file accordingly

--- ports/freertos-posix/toolchain.cmake
+++ ports/freertos-posix/toolchain.cmake
@@ -1,28 +1,19 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR "${CMAKE_SYSTEM_HOST_PROCESSOR}")
 
-set(root "${CMAKE_CURRENT_LIST_DIR}")
-set(workspace "${root}/workspace")
-set(lwip "${workspace}/bsp/lwip")
-set(lwip_contrib "${workspace}/bsp/lwip-contrib")
-set(freertos "${root}/ports/freertos-posix")
-set(user_include "${workspace}/bsp")
+set(include_paths "-I${CMAKE_CURRENT_LIST_DIR}/lwip -I${CMAKE_CURRENT_LIST_DIR}/FreeRTOS-Sim/Source/include -I${CMAKE_CURRENT_LIST_DIR}/include -I${CMAKE_CURRENT_LIST_DIR}/build/install/FreeRTOS-Sim/include -I${CMAKE_CURRENT_LIST_DIR}/lwip/lwip-contrib/ports/unix/lib -I${CMAKE_CURRENT_LIST_DIR}/lwip/lwip-contrib/ports/unix/port/include -I${CMAKE_CURRENT_LIST_DIR}/lwip/lwip/src/include")
 
-set(include_paths "-I${freertos}/FreeRTOS-Sim/Source/include -I${freertos}/include -I${freertos}/build/install/FreeRTOS-Sim/include -I${user_include} -I${lwip_contrib}/ports/unix/lib -I${lwip_contrib}/ports/unix/port/include -I${lwip}/src/include")
-
-set(library_paths "-L${freertos}/build/install/FreeRTOS-Sim/lib -L${lwip_contrib}/ports/unix/example_app/build")
+set(library_paths "-L${CMAKE_CURRENT_LIST_DIR}/build/install/FreeRTOS-Sim/lib -L${CMAKE_CURRENT_LIST_DIR}/lwip/lwip-contrib/ports/unix/example_app/build")
 
 set(c_libraries "-lfreertos-sim -lfreertos-sim-loader -lpthread -llwipcore -llwipcontribportunix")
 set(cxx_libraries "${c_libraries}")
-
-set(objects "${root}/loader/init.o ${root}/loader/tls.o")
 
 set(CMAKE_C_COMPILER "/usr/bin/gcc")
 set(CMAKE_CXX_COMPILER "/usr/bin/g++")
 
 set(CMAKE_C_FLAGS "${include_paths} ${c_flags} ${defines}")
 set(CMAKE_CXX_FLAGS "${include_paths} ${cxx_flags} ${defines}")
-set(CMAKE_EXE_LINKER_FLAGS "${linker_flags} ${library_paths} -Wl,-rpath -Wl,${lwip_contrib}/ports/unix/example_app/build")
+set(CMAKE_EXE_LINKER_FLAGS "${linker_flags} ${library_paths} -Wl,-rpath -Wl,${CMAKE_CURRENT_LIST_DIR}/lwip/lwip-contrib/ports/unix/example_app/build")
 set(CMAKE_C_STANDARD_LIBRARIES "${c_objects} ${library_paths} ${c_libraries}")
 set(CMAKE_CXX_STANDARD_LIBRARIES "${cxx_objects} ${library_paths} ${cxx_libraries}")
 
